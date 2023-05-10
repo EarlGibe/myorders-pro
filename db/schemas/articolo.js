580d50938a8b6697a72d2842d06421e3bfb2aad4
Taba2812 <gabriele.tabarellidefatis@gmail.com>
2023-05-10 20:17:39 +0200

    Complete articolo

--- db/schemas/articolo.js
+++ db/schemas/articolo.js
@@ -2,13 +2,29 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const articoloSchema = new Schema({
-    id: Number,
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    nome: {
+        type: String,
+        required: true
+    },
     descrizione: String,
     coloriDisponibili: Array,
     taglieDisponibili: Array,
+    materiale: String,
+    paese: String,
     scontoApplicato: Number,
-    prezzo: Number,
-    stato: Boolean
+    prezzo: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Articolo', articoloSchema);
