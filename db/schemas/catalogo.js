f4f46a63b48a7cc4770577685d9fe2c94a6437ab
Taba2812 <gabriele.tabarellidefatis@gmail.com>
2023-05-10 19:48:19 +0200

    Rename directory

--- db/schemas/catalogo.js
+++ db/schemas/catalogo.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
+const Articolo = require('./articoloSchema');
 
 const cataloghiSchema = new Schema({
     id: Number,
     articoli: Array,
     status: Boolean
 });
+
+module.exports = mongoose.model('Catalogo', articoliSchema);
