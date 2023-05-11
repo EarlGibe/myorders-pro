eae1f5d9f0ef5f20d08d1101f66d89e2f5fc878a
EarlGibe <g.tabarellidefatis-1@studenti.unitn.it>
2023-05-11 15:49:15 +0200

    Add index and package

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const app = require('./app/app.js');
-const mongoose = require('mongoose');
+const express = require("express");
+var app = express();
 
-/**
- * https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
- */
-const port = process.env.PORT || 8080;
+const httpPort = 3000;
 
-
-/**
- * Configure mongoose
- */
-// mongoose.Promise = global.Promise;
-app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then ( () => {
-    
-    console.log("Connected to Database");
-    
-    app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-    });
-    
+app.get('/', (req, res) => {
+    res.sendStatus(404);
 });
+
+app.post('/', (req, res) => {
+    res.send('post');
+});
+
+app.put('/', (req, res) => {
+    res.send('put');
+});
+
+app.delete('/', (req, res) => {
+    res.send('delete');
+});
+
+app.listen(httpPort, function(){
+    console.log('Server running on port: ', httpPort);
+})
+
+// const app = require('./app/app.js');
+// const mongoose = require('mongoose');
+
+// /**
+//  * https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
+//  */
+// const port = process.env.PORT || 8080;
+
+
+// /**
+//  * Configure mongoose
+//  */
+// // mongoose.Promise = global.Promise;
+// app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+// .then ( () => {
+    
+//     console.log("Connected to Database");
+    
+//     app.listen(port, () => {
+//         console.log(`Server listening on port ${port}`);
+//     });
+    
+// });
