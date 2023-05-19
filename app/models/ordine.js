14182f25c93b752a1eb83db670ea0932d4fa80e5
PhilosHipposComtes <filippo.conti@studenti.unitn.it>
2023-05-19 13:23:50 +0200

    Correct ordine schema version 2

--- app/models/ordine.js
+++ app/models/ordine.js
@@ -5,11 +5,11 @@ const ordineSchema = new Schema({
   id: [{type: Number, required: true }],
   cliente: { type: Schema.Types.ObjectId, ref: 'Cliente', required: true },
   subagente: { type: Schema.Types.ObjectId, ref: 'Subagente', required: true },
-  dataInserimento: { type: Date, default: Date.now },
   listaArticoli: [{ type: Schema.Types.ObjectId, ref: 'Articolo', required: true }],
   indirizzoSpedizione: { type: String, required: true },
   indirizzoFatturazione: { type: String, required: true },
-  status: { type: Boolean, default: falses }
+  status: { type: Boolean, default: falses },
+  dataInserimento: { type: Date, default: Date.now }
 },
 {
   collection:'ordini'
