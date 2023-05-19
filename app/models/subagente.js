566661933226140de09c73768707f613e2a8fde8
PhilosHipposComtes <filippo.conti@studenti.unitn.it>
2023-05-19 13:10:24 +0200

    Correct subagente schema

--- app/models/subagente.js
+++ app/models/subagente.js
@@ -4,11 +4,11 @@ var Schema = mongoose.Schema;
 const subagenteSchema = new Schema({
   id: [{type: Number, required: true }],
   matricola: [{type: String, required: true }],
+  dati: {type: Schema.Types.ObjectId, ref: 'Anagrafica'},
   listaOrdini: { type: Schema.Types.ObjectId, ref: 'Ordine', required: false },
   listaClienti: { type: Schema.Types.ObjectId, ref: 'Cliente', required: false },
   listaAziende: { type: Schema.Types.ObjectId, ref: 'Azienda', required: true },
-  dataInserimento: { type: Date, default: Date.now },
-  status: { type: Boolean, default: false }
+  dataInserimento: { type: Date, default: Date.now }
 },
 {
   collection:'subagenti'
