612a76b73a9f419b47286a4445cf94cc5430d3f0
PhilosHipposComtes <filippo.conti@studenti.unitn.it>
2023-05-19 13:16:24 +0200

    Correct user schema

--- app/models/user.js
+++ app/models/user.js
@@ -2,11 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const userSchema=new Schema({
+    id: [{type: Number, required: true }],
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
     role: { type: String, required: true },
     isActive: { type: Boolean, default: false }
+    isFirstAccess: { type: Boolean, default: true },
+    loggedIn: { type: Boolean, default: false },
+    dataInserimento: { type: Date, default: Date.now }
 },
 {
     collection:'users'
