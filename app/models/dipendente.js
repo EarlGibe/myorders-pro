3328d4d0b39c69b214f8f3080d13dae65f83f247
PhilosHipposComtes <filippo.conti@studenti.unitn.it>
2023-05-19 13:08:41 +0200

    Correct dipendenti schema

--- app/models/dipendente.js
+++ app/models/dipendente.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const dipendenteSchema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  status: { type: Boolean, default: false },
-  loggedIn: { type: Boolean, default: false },
-  isFirstAccess: { type: Boolean, default: true },
+  id: [{type: Number, required: true }],
   matricola: { type: Number, required: true },
-  dati: {type: Schema.Types.ObjectId, ref: 'Anagrafica'}
-  
-}, { collection: 'dipendenti' });
+  dati: {type: Schema.Types.ObjectId, ref: 'Anagrafica'},
+  dataInserimento: { type: Date, default: Date.now }
+},
+{
+   collection: 'dipendenti'
+});
 
 const Dipendente = mongoose.model('Dipendente', dipendenteSchema);
 
