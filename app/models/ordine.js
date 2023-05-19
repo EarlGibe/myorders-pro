f9db9eea3515709bbcafb4c8b0e1b9737ea062d4
PhilosHipposComtes <filippo.conti@studenti.unitn.it>
2023-05-19 13:09:30 +0200

    Correct ordine schema

--- app/models/ordine.js
+++ app/models/ordine.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const ordineSchema = new Schema({
+  id: [{type: Number, required: true }],
   cliente: { type: Schema.Types.ObjectId, ref: 'Cliente', required: true },
   subagente: { type: Schema.Types.ObjectId, ref: 'Subagente', required: true },
-  data: { type: Date, default: Date.now },
+  dataInserimento: { type: Date, default: Date.now },
   listaArticoli: [{ type: Schema.Types.ObjectId, ref: 'Articolo', required: true }],
   indirizzoSpedizione: { type: String, required: true },
   indirizzoFatturazione: { type: String, required: true },
