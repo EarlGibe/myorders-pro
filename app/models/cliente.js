3cd8dab021239672a242153b6fc4ae912eeb0a7b
PhilosHipposComtes <filippo.conti@studenti.unitn.it>
2023-05-19 13:07:28 +0200

    Correct clienti schema

--- app/models/cliente.js
+++ app/models/cliente.js
@@ -5,8 +5,8 @@ const clienteSchema = new Schema({
   id: [{type: Number, required: true }],
   anagrafica: { type: Schema.Types.ObjectId, ref: 'Anagrafica', required: true },
   subagente: { type: Schema.Types.ObjectId, ref: 'Subagente', required: true },
-  dataInserimento: { type: Date, default: Date.now },
-  status: { type: Boolean, default: false }
+  status: { type: Boolean, default: false },
+  dataInserimento: { type: Date, default: Date.now }  
 },
 {
   collection:'clienti'
