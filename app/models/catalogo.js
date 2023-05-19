6c3a794a0b92b9a4f9994ed020d6f640ffad82aa
PhilosHipposComtes <filippo.conti@studenti.unitn.it>
2023-05-19 13:06:09 +0200

    Correct catalog schema

--- app/models/catalogo.js
+++ app/models/catalogo.js
@@ -6,7 +6,8 @@ const catalogoSchema = new Schema({
   listaArticoli: [{ type: Schema.Types.ObjectId, ref: 'Articolo', required: true }],
   azienda: { type: Schema.Types.ObjectId, ref: 'Azienda', required: true },
   dataInserimento: { type: Date, default: Date.now },
-  status: { type: Booolean, default: false }
+  status: { type: Booolean, default: false },
+  dataInserimento: { type: Date, default: Date.now }
 },
 {
   collection:'cataloghi'
