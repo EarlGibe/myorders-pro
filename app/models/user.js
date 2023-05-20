8903024dce95accb0970bea4f5543a6909a0195c
robertogiordano <roberto.giordano@studenti.unitn.it>
2023-05-20 14:36:56 +0200

    user model corrected

--- app/models/user.js
+++ app/models/user.js
@@ -7,7 +7,7 @@ const userSchema=new Schema({
     password: { type: String, required: true },
     email: { type: String, required: true },
     role: { type: String, required: true },
-    isActive: { type: Boolean, default: false }
+    isActive: { type: Boolean, default: false },
     isFirstAccess: { type: Boolean, default: true },
     loggedIn: { type: Boolean, default: false },
     dataInserimento: { type: Date, default: Date.now }
