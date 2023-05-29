b79de3c6c59fa1092ad2a4e469565be486d0791e
robertogiordano <roberto.giordano@studenti.unitn.it>
2023-05-29 22:21:16 +0200

    display all articles for ordine Template

--- static/creaOrdine/script.js
+++ static/creaOrdine/script.js
@@ -3,7 +3,7 @@ var userId = localStorage.getItem("userId");
 var subagente = JSON.parse(localStorage.getItem("subagente"));
 
 
-function onLoadCreaOrdineIndex(){
+function onLoadCreaOrdineIndex() {
     // Code to be executed when the page loads
     getAllClienti();
     // Get all the anchor elements within the 'buttons' div
@@ -25,12 +25,12 @@ function getAllClienti() {
             'x-access-token': token
         }
     })
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) { // Here you get the data to modify as you please
-        console.log(data);
-        populateClienti(data);
-    })
-    .catch(error => console.error(error)); // If there is any error, you will catch them here
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) { // Here you get the data to modify as you please
+            console.log(data);
+            populateClienti(data);
+        })
+        .catch(error => console.error(error)); // If there is any error, you will catch them here
 }
 
 function populateClienti(clienti) {
@@ -41,24 +41,24 @@ function populateClienti(clienti) {
 
     // Add options from the retrieved data
     for (var i = 0; i < clienti.length; i++) {
-        if(subagente.listaClienti.includes(clienti[i]._id)){
+        if (subagente.listaClienti.includes(clienti[i]._id)) {
             var option = document.createElement("option");
             option.value = clienti[i]._id;
-            option.text = clienti[i].anagrafica.nome+" "+clienti[i].anagrafica.cognome; // Assuming the "nome" property holds the name of the cliente
+            option.text = clienti[i].anagrafica.nome + " " + clienti[i].anagrafica.cognome; // Assuming the "nome" property holds the name of the cliente
             selectCliente.appendChild(option);
         }
-        
+
     }
 }
 
-function creaOrdine(){
+function creaOrdine() {
 
     localStorage.setItem("clienteSelezionato", document.getElementById("clienteSelezionato").value);
-    console.log("cliente: "+document.getElementById("clienteSelezionato").value);
+    console.log("cliente: " + document.getElementById("clienteSelezionato").value);
 
-    document.getElementById("ordineForm").addEventListener("click", function(event){
+    document.getElementById("ordineForm").addEventListener("click", function (event) {
         event.preventDefault();
-      });
+    });
 
 
     fetch('../ordini', {
@@ -67,26 +67,26 @@ function creaOrdine(){
             'x-access-token': token,
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify( { subagente: subagente._id, cliente: document.getElementById("clienteSelezionato").value } ),
+        body: JSON.stringify({ subagente: subagente._id, cliente: document.getElementById("clienteSelezionato").value }),
     })
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) { // Here you get the data to modify as you please
-        console.log(data);
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) { // Here you get the data to modify as you please
+            console.log(data);
 
-        redirectToGetAllAziende();
-    })
-    .catch(error => console.error(error)); // If there is any error, you will catch them here*/
+            redirectToGetAllAziende();
+        })
+        .catch(error => console.error(error)); // If there is any error, you will catch them here*/
 
-    
+
 }
 
-function redirectToGetAllAziende(){
+function redirectToGetAllAziende() {
 
-    window.location.href='./aziendeDisponibili.html';
+    window.location.href = './aziendeDisponibili.html';
 }
 
-function getAllAziende(){
-    
+function getAllAziende() {
+
 
     fetch('../aziende', {
         method: 'GET',
@@ -94,133 +94,176 @@ function getAllAziende(){
             'x-access-token': token
         }
     })
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) { // Here you get the data to modify as you please
-        console.log(data);
-        populateAziende(data);
-    })
-    .catch(error => console.error(error)); // If there is any error, you will catch them here
-    
-    
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) { // Here you get the data to modify as you please
+            console.log(data);
+            populateAziende(data);
+        })
+        .catch(error => console.error(error)); // If there is any error, you will catch them here
+
+
 }
 
-function populateAziende(aziende){
+function populateAziende(aziende) {
     const aziendeList = document.getElementById("aziendeList");
     // Popolamento dell'elenco delle aziende
     aziende.forEach(azienda => {
-        if(subagente.listaAziende.includes(azienda._id)){
+        if (subagente.listaAziende.includes(azienda._id)) {
             const listItem = document.createElement("li");
             listItem.textContent = azienda.dati.nome;
-        
+
             const selectButton = document.createElement("button");
             selectButton.textContent = "Seleziona";
             selectButton.addEventListener("click", () => {
-            // Logica per gestire la selezione dell'azienda
+                // Logica per gestire la selezione dell'azienda
                 console.log("Azienda selezionata:", azienda._id);
-                localStorage.setItem("aziendaSelezionata",JSON.stringify(azienda))
-                window.location.href='./cataloghiDisponibili.html';
-            
+                localStorage.setItem("aziendaSelezionata", JSON.stringify(azienda))
+                window.location.href = './cataloghiDisponibili.html';
+
             });
-        
+
             listItem.appendChild(selectButton);
             aziendeList.appendChild(listItem);
         }
-        
-      });    
+
+    });
 }
 
-function getAllCataloghi(){
+function getAllCataloghi() {
 
-    var azienda=JSON.parse(localStorage.getItem("aziendaSelezionata"));
+    var azienda = JSON.parse(localStorage.getItem("aziendaSelezionata"));
 
     console.log(azienda);
-    
+
     azienda.listaCataloghi.forEach(catalogo_id => {
-        fetch('../cataloghi/'+catalogo_id, {
+        fetch('../cataloghi/' + catalogo_id, {
             method: 'GET',
             headers: {
                 'x-access-token': token
             }
         })
-        .then((resp) => resp.json()) // Transform the data into json
-        .then(function(data) { // Here you get the data to modify as you please
-            console.log(data);
-            populateCataloghi(data);
-        })
-        .catch(error => console.error(error)); // If there is any error, you will catch them here
+            .then((resp) => resp.json()) // Transform the data into json
+            .then(function (data) { // Here you get the data to modify as you please
+                console.log(data);
+                populateCataloghi(data);
+            })
+            .catch(error => console.error(error)); // If there is any error, you will catch them here
     })
-    
-    
-    
+
+
+
 }
 
-function populateCataloghi(catalogo){
+function populateCataloghi(catalogo) {
 
     const cataloghiList = document.getElementById("cataloghiList");
     // Popolamento dell'elenco delle cataloghi
-   
-        const listItem = document.createElement("li");
-        listItem.textContent = catalogo.nome;
-      
-        const selectButton = document.createElement("button");
-        selectButton.textContent = "Seleziona";
-        selectButton.addEventListener("click", () => {
-          // Logica per gestire la selezione dell'catalogo
-            console.log("Catalogo selezionato:", catalogo._id);
-            localStorage.setItem("catalogoSelezionato",JSON.stringify(catalogo))
-            window.location.href='./articoliDisponibili.html';
-         
-        });
-      
-        listItem.appendChild(selectButton);
-        cataloghiList.appendChild(listItem);
- 
+
+    const listItem = document.createElement("li");
+    listItem.textContent = catalogo.nome;
+
+    const selectButton = document.createElement("button");
+    selectButton.textContent = "Seleziona";
+    selectButton.addEventListener("click", () => {
+        // Logica per gestire la selezione dell'catalogo
+        console.log("Catalogo selezionato:", catalogo._id);
+        localStorage.setItem("catalogoSelezionato", JSON.stringify(catalogo))
+        window.location.href = './articoliDisponibili.html';
+
+    });
+
+    listItem.appendChild(selectButton);
+    cataloghiList.appendChild(listItem);
+
 }
 
-function getAllArticoli(){
+function getAllArticoli() {
 
-    var catalogo=JSON.parse(localStorage.getItem("catalogoSelezionato"));
+    var catalogo = JSON.parse(localStorage.getItem("catalogoSelezionato"));
 
     console.log(catalogo);
-    
+
     catalogo.listaArticoli.forEach(articolo_id => {
-        fetch('../articoli/'+articolo_id, {
+        fetch('../articoli/' + articolo_id, {
             method: 'GET',
             headers: {
                 'x-access-token': token
             }
         })
-        .then((resp) => resp.json()) // Transform the data into json
-        .then(function(data) { // Here you get the data to modify as you please
-            console.log(data);
-            populateArticoli(data);
-        })
-        .catch(error => console.error(error)); // If there is any error, you will catch them here
+            .then((resp) => resp.json()) // Transform the data into json
+            .then(function (data) { // Here you get the data to modify as you please
+                console.log(data);
+                populateArticoli(data);
+            })
+            .catch(error => console.error(error)); // If there is any error, you will catch them here
     })
-    
-    
-    
+
+
+
 }
 
-function populateArticoli(articolo){
+function populateArticoli(article) {
 
-    const articoliList = document.getElementById("articoliList");
-    // Popolamento dell'elenco delle articolo
+    console.log("populate");
+
+    const productTable = document.getElementById("productTable");
+
+    //const row = document.createElement("tr");
+    /*row.innerHTML = `
+            <td rowspan="${article.coloriDisponibili.length}">
+                ${article.nome}
+            </td>
+            <td rowspan="${article.coloriDisponibili.length}">
+                ${article.descrizione}
+            </td>
+            `*/
+
+    // Creazione delle righe per ogni colore e taglia disponibile
+    article.coloriDisponibili.forEach(color => {
+
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td >
+                ${article.nome}
+            </td>
+            <td >
+                ${article.descrizione}
+            </td>
+            `
+
+        row.innerHTML += ` <td>${color}</td>`
+
+        article.taglieDisponibili.forEach(taglia => {
+            row.innerHTML += `
+                        <td>${taglia}</td>
+                        <td>
+                            <select>
+                                <option value="0">0</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <!-- Add more quantity options if needed -->
+                            </select>
+                        </td>
+                       
+                `;
+            });
+
+            row.innerHTML += 
+            `
+                <td>
+                    ${article.scontoApplicato}
+                </td>
+                <td>
+                    ${article.prezzo}
+                </td>
+                <td>
+                    ${article.barCodes.join(", ")}
+                </td>
+            `
+            productTable.appendChild(row);
+    });
+
    
-        const listItem = document.createElement("li");
-        listItem.textContent = articolo.nome;
-      
-        const selectButton = document.createElement("button");
-        selectButton.textContent = "Seleziona";
-        selectButton.addEventListener("click", () => {
-          // Logica per gestire la selezione dell'catalogo
-            console.log("Articolo selezionato:", articolo._id);
-            localStorage.setItem("articoloSelezionato",JSON.stringify(articolo))
-            //window.location.href='./articoliDisponibili.html';
-         
-        });
-      
-        listItem.appendChild(selectButton);
-        articoliList.appendChild(listItem);
- 
+
 }
