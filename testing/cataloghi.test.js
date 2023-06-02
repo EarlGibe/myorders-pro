95dbcd13eb8a67ee70b6517c8bce9bc21cc8515a
PhilosHipposComtes <filippo.conti@studenti.unitn.it>
2023-06-02 13:33:54 +0200

    Add cataloghi testis

---
+++ testing/cataloghi.test.js
@@ -0,0 +1,89 @@
+// imports section
+const request = require('supertest');
+const app = require('../app/app');
+const mongoose = require('mongoose');
+
+// Database section
+const dbuser = 'Group19';
+const dbpassword = 'BDqYxCkjxOx5lWA0';
+const dbname = 'myorders_pro';
+const dbhost = 'maincluster.yx3zxsu.mongodb.net';
+const dbparams = 'retryWrites=true&w=majority';
+const dbURL = `mongodb+srv://${dbuser}:${dbpassword}@${dbhost}/${dbname}?${dbparams}`;
+
+// token section
+const passepartout = 'supercalifragilistichespiralitoso';
+
+// API url
+const apiName = 'cataloghi';
+const apiURL = '/' + apiName;
+
+// settings
+const timeout = 8000;
+
+// documents IDs
+const getID = '646a20c4d322a15436346aa0';
+const putID = '646a20c4d322a15436346aa0';
+const deleteID = '6479d378cf6294b91cc9fd72';
+
+describe('[Testing] ' + apiName, () => {
+  beforeAll( async () => { jest.setTimeout(timeout);
+
+  app.locals.db = await mongoose.connect(dbURL); });
+
+  afterAll( () => { mongoose.connection.close(true); });
+
+  test('GET ' + apiURL + ' should respond with 200', async () => {
+    return request(app).get(apiURL)
+    .set('x-access-token', passepartout).set('Accept', 'application/json')
+    .expect(200);
+  });
+
+  test('GET ' + apiURL + '/{id} should respond with 200', async () => {
+    return request(app).get(apiURL + '/' + getID)
+    .set('x-access-token', passepartout).set('Accept', 'application/json')
+    .expect(200);
+  });
+
+  test('POST ' + apiURL + ' should respond with 200', () => {
+    return request(app).post(apiURL)
+    .set('x-access-token', passepartout).set('Accept', 'application/json')
+
+    // set post data
+
+    .expect(200);
+  });
+
+  test('PUT ' + apiURL + ' should respond with 200', () => {
+    return request(app).put(apiURL)
+    .set('x-access-token', passepartout).set('Accept', 'application/json')
+
+    // set put data
+
+    .expect(200);
+  });
+
+  test('PUT ' + apiURL + '/{id} should respond with 200', async () => {
+    return request(app).put(apiURL + '/' + putID)
+    .set('x-access-token', passepartout).set('Accept', 'application/json')
+
+    // set put data 
+
+    .expect(200);
+  });
+
+  // // !!!!!!! Questo comando ELIMINA TUTTO !!!!!!!
+  // // -----------------------------------------------------
+  // test('DELETE ' + apiURL + ' should respond with 200', async () => {
+  //   return request(app).delete(apiURL)
+  //   .expect(200);
+  // });
+
+  // Elimina il documento selezionato. assicurarsi di averlo presente
+  // ------------------------------------------------------------
+  test('DELETE ' + apiURL + '/{id} should respond with 200', async () => {
+    return request(app).delete(apiURL + '/' + deleteID)
+    .expect(200);
+  });
+  
+});
