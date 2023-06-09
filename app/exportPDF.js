7f8ac0a9bdf1367b1339aa7d518043068dc4b4e7
Robertogiordano <107034283+Robertogiordano@users.noreply.github.com>
2023-06-10 00:48:50 +0200

    Update exportPDF.js

--- app/exportPDF.js
+++ app/exportPDF.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const sgMail = require('@sendgrid/mail')
 
 async function exportTableToPdf(html, outputFilePath) {
